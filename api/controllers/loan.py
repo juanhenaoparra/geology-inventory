5ad4ac7e6bd24d2c86f5d8e2599fe33d40fcdda5
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-11-11 10:55:21 -0500

    Endpoint agregado para obtener historial de todos los prestamos

--- api/controllers/loan.py
+++ api/controllers/loan.py
@@ -5,19 +5,45 @@ from sqlmodel import Session
 from sqlalchemy import select
 from fastapi.encoders import jsonable_encoder
 from controllers.exceptions import UserError
-from models.models import LoanStock, Loan
+from models.models import LoanStock, Loan, User, UserLoan
 
 logger = logging.getLogger(__name__)
 
 def tool_loan_history(session: Session, tool_id: int):
     """Get the loan history for a tool"""
     try:
-        loans = session.exec(
+        tool_loans = session.exec(
             select(Loan).join(LoanStock).where(LoanStock.stock_id == tool_id)
         ).all()
-        return [jsonable_encoder(loan[0].dict()) for loan in loans]
+        return [jsonable_encoder(loan[0].dict()) for loan in tool_loans]
     except Exception as exc:
         logger.error("An unexpected error occurred", exc_info=True)
         raise UserError(
             detail="An unexpected error occurred while getting tool loan history."
+        ) from exc
+
+def loans(session: Session):
+    """Get all loans"""
+    try:
+        # Get all loans with their relationships
+        all_loans = session.exec(
+            select(Loan, User)
+            .join(UserLoan, Loan.id == UserLoan.loan_id)
+            .join(User, UserLoan.user_id == User.id)
+            .join(LoanStock, Loan.id == LoanStock.loan_id)
+        ).all()
+
+        # Include the user name in the loans
+        loans_with_his_user = []
+        for loan, user in all_loans:
+            loan_dict = loan.dict()
+            loan_dict["user_name"] = user.name
+            loans_with_his_user.append(loan_dict)
+
+        return loans_with_his_user
+
+    except Exception as exc:
+        logger.error("An unexpected error occurred", exc_info=True)
+        raise UserError(
+            detail="An unexpected error occurred while getting all loans."
         ) from exc
--- api/routes/router.py
+++ api/routes/router.py
@@ -7,7 +7,7 @@ from database import get_session
 from models.models import Stock
 from controllers.stock import store_stock
 from controllers.exceptions import UserError
-from controllers.loan import tool_loan_history
+from controllers.loan import tool_loan_history, loans
 
 router = APIRouter()
 
@@ -31,6 +31,10 @@ def get_tool_loan_history(tool_id: int, session: Session = Depends(get_session))
     """Get the loan history for a tool"""
     return tool_loan_history(session=session, tool_id=tool_id)
 
+@router.get("/loans")
+def get_loans(session: Session = Depends(get_session)):
+    """Get all loans"""
+    return loans(session=session)
 
 @router.post("/stock")
 def post_store_stock(stock: Stock, session: Session = Depends(get_session)):
