f42b8d637acc31c3f913470b2beb496085d8c0a3
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-11-26 22:46:56 -0500

    Variables de entorno corregidas

--- frontend/src/services/AuthService.ts
+++ frontend/src/services/AuthService.ts
@@ -1,7 +1,5 @@
 // @ts-ignore
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID
-// @ts-ignore
-const BASE_API_HOST = import.meta.env.VITE_API_HOST
 
 export const initiateGoogleLogin = () => {
     const googleAuthUrl = 'https://accounts.google.com/o/oauth2/v2/auth'
@@ -10,7 +8,7 @@ export const initiateGoogleLogin = () => {
     const params = {
         client_id: GOOGLE_CLIENT_ID,
         redirect_uri: redirectUri,
-        response_type: 'token',
+        response_type: 'code',
         scope: 'openid email profile',
         access_type: 'offline',
         prompt: 'consent',
