458491efb9cba67f3e2a3a8efb80eb13b1132066
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-11-28 20:35:03 -0500

    Usuario se guarda en estado global

--- frontend/src/globalStates/useUserStore.ts
+++ frontend/src/globalStates/useUserStore.ts
@@ -2,13 +2,13 @@ import { create } from 'zustand'
 import { User } from '@/models/business/user.model'
 
 interface UserState {
-    user: User | null
+    user: User
     setUser: (user: User) => void
     clearUser: () => void
 }
 
 export const useUserStore = create<UserState>((set) => ({
-    user: null,
+    user: {} as User,
     setUser: (user) => set({ user }),
-    clearUser: () => set({ user: null }),
+    clearUser: () => set({ user: {} as User }),
 }))
