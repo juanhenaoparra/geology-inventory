fb0f2d393e02e41ca2ca193005cff9fb1562facd
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-11-28 23:10:31 -0500

    Comentario innecesario es retirado

--- api/main.py
+++ api/main.py
@@ -16,7 +16,7 @@ app = FastAPI(lifespan=lifespan)
 
 # Configurar CORS
 origins = [
-    "http://localhost:3000",  # Tu frontend React
+    "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
 
