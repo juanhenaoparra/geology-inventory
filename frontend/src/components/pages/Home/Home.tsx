8e25c29e9061ac1b3bf14109723966dbfff02c5c
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-11-28 21:08:50 -0500

    Boton de logout es agregado

--- frontend/src/components/pages/Home/Home.tsx
+++ frontend/src/components/pages/Home/Home.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Button } from '@/components/ui/button'
+import { useUserStore } from '@/globalStates/useUserStore'
+import { useNavigate } from 'react-router-dom'
 
 const Home: React.FC = () => {
+    const { user, clearUser } = useUserStore()
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        clearUser()
+        navigate('/login')
+    }
+
+    useEffect(() => {
+        if (!user?.id) {
+            navigate('/login')
+        }
+    }, [user, navigate])
+
     return (
-        <div>
-            <h1 className="text-2xl font-bold">Bienvenido al Sistema de Inventario</h1>
-            <p className="mt-2">Selecciona una opción del menú superior.</p>
-        </div>
+        <main className='flex justify-between'>
+            <section>
+                <h1 className="text-2xl font-bold">Bienvenido al Sistema de Inventario</h1>
+                <p className="mt-2">Selecciona una opción del menú superior.</p>
+            </section>
+            <section>
+                <Button onClick={handleLogout}>Cerrar Sesión</Button>
+            </section>
+        </main>
     )
 }
 
